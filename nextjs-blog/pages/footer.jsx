7039c22e3123b3dd65158612a7c1e18be3adca9d
Lucas <lucasferguson16@gmail.com>
2024-03-27 18:54:01 -0500

    Co-authored-by: muymay <muymay@users.noreply.github.com> Co-authored-by: Astr-Beas <Astr-Beas@users.noreply.github.com> Co-authored-by: Joseph Pancho <janchpanch@users.noreply.github.com>

---
+++ nextjs-blog/pages/footer.jsx
@@ -0,0 +1,13 @@
+// React Component for the footer of the website
+
+export default function Footer() {
+
+	console.log('Hello!');
+
+	return <footer>
+		<a>
+			Powered by ME MYSELF AND I
+			<img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+		</a>
+	</footer>;
+}
---
+++ nextjs-blog/pages/header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+	return <div>
+		<h1>Header of our site!!!</h1>
+	</div>;
+}
+
+
+
+// jsx
