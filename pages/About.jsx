25620725109584913667eba04ce8564d8c629f4e
muymay <melissalaiz4@gmail.com>
2024-04-02 02:22:00 -0500

    added background
    fixing this tomorrow

--- pages/About.jsx
+++ pages/About.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from '../styles/Home.module.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Header from './header';
-
+import Background from './Background';
 
 const AboutMe = () => {
   const people = [
@@ -39,11 +39,11 @@ const AboutMe = () => {
   return (
     <div className={styles.homepage}>
       <Header />
-
+      <Background />
       <div></div>
-    <div className={styles['about-me-container']}>
+      <div className={styles['about-me-container']}>
       <div className={styles['about-us']}>
-        <h2>About Our Team</h2>
+        <h2>About Us</h2>
         <p>This is a brief description of our team or organization.</p>
       </div>
       <div className={styles['people-list']}>
@@ -56,8 +56,9 @@ const AboutMe = () => {
               <h3>{person.name}</h3>
               <p>{person.description}</p>
               <div className={styles['social-links']}>
-                <a href={person.linkedIn} target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
-                <a href={person.github} target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+                <a href={person.linkedIn} target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin fa-lg"></i></a>
+                <span style={{ marginLeft: '10px' }}></span> {/* Add spacing */}
+                <a href={person.github} target="_blank" rel="noopener noreferrer"><i className="fab fa-github fa-lg"></i></a>
               </div>
             </div>
           </div>
