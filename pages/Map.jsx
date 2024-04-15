ba5101a0cf8bb2778aed7774f1a0ba1e2d6936f6
Lucas <lucasferguson16@gmail.com>
2024-04-14 20:07:37 -0500

    not working code

--- pages/Map.jsx
+++ pages/Map.jsx
@@ -2,14 +2,51 @@
 import Header from './header';
 import Background from './Background';
 import Footer from './footer';
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import DeckGL from '@deck.gl/react';
+import { StaticMap } from 'react-map-gl';
+import { ScatterplotLayer } from '@deck.gl/layers';
 
 import { env } from '../env';
 
 const MAPBOX_ACCESS_TOKEN = env.MAPBOX_API_KEY;
 
-let env = {
-    MAPBOX_API_KEY: "pk.eyJ1IjoiYWx3YXlzc3RheXBvc2l0aXZlMTIzIiwiYSI6ImNsdjA3NmZ4YzFmNTcycW1rc2todGw1aWkifQ.mL16OyuXjhpXe5FVcZmaSg"
-}
+// Set your initial map view state
+const initialViewState = {
+    longitude: -74.5,
+    latitude: 40,
+    zoom: 9,
+    pitch: 0,
+    bearing: 0
+};
 
-export { env };
+// Data for the scatterplot layer
+const data = [{ position: [-74.5, 40], size: 100 }];
+
+// Define the scatterplot layer
+const scatterplotLayer = new ScatterplotLayer({
+    id: 'scatterplot-layer',
+    data: data,
+    getPosition: d => d.position,
+    getRadius: d => d.size,
+    getColor: [255, 0, 0], // Red color
+    opacity: 0.8,
+    pickable: true
+});
+
+const MapComponent = () => {
+    return (
+        <DeckGL
+            initialViewState={initialViewState}
+            controller={true}
+            layers={[scatterplotLayer]}
+        >
+            <StaticMap
+                mapStyle="mapbox://styles/mapbox/streets-v11"
+                mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
+            />
+        </DeckGL>
+    );
+};
+
+export default MapComponent;
